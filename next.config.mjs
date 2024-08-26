fb1c6704da1e2f4abccc84a39a39e3bea63461d3
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2024-08-26 15:01:33 +0200

    chore: vercel json

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    "@nillion/client-core",
-    "@nillion/client-react-hooks",
-    "@nillion/client-vms",
-  ],
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
@@ -23,44 +18,24 @@ const nextConfig = {
 
     return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+        ],
+      },
+    ];
   },
-  headers: [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Cross-Origin-Opener-Policy",
-          value: "same-origin",
-        },
-        {
-          key: "Cross-Origin-Embedder-Policy",
-          value: "require-corp",
-        },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "*",
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value: "X-Requested-With, content-type, Authorization",
-        },
-      ],
-    },
-  ],
   async rewrites() {
     return [
       {
-        source: "/nilchain-proxy/:path*",
-        destination: `${process.env.NEXT_PUBLIC_NILLION_NILCHAIN_JSON_RPC}/:path*`,
+        source: "/nilchain",
+        destination: "http://127.0.0.1:48102/",
       },
     ];
   },
 };
-
 export default nextConfig;
